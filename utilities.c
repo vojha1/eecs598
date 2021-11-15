c67ba703054651d86b1e82bf3b360d50b1a37401
vojha1 <47861518+vojha1@users.noreply.github.com>
2021-11-15 16:05:21 -0500

    Add files via upload
    Function to create the weight 2D array is changed so that it is contiguous

--- utilities.c
+++ utilities.c
@@ -202,8 +202,10 @@ parameter_t parameter_parser(int input_nodes, int output_nodes, char* weight_f, 
 
 	parameter_c.biasses = (float*) malloc (output_nodes * sizeof(float));
 	parameter_c.weights = (float**) malloc (input_nodes * sizeof(float*));
-	for (i = 0; i < input_nodes; ++i) {
-		parameter_c.weights[i] = (float*) malloc (output_nodes * sizeof(float));
+	float *t0 = malloc(input_nodes * output_nodes * sizeof (float));  //New array which is contigious
+	for (i = 0; i < input_nodes; ++i) 
+	{
+		parameter_c.weights[i] = t0 + i*output_nodes ; //(float*) malloc (output_nodes * sizeof(float));
 	}
 	parameter_c.in_feature_num = input_nodes;
 	parameter_c.out_feature_num = output_nodes;
