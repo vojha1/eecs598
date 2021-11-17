bfa2e1481c65109f3d326e30c87f3ecfad9b8902
Wei-Fan Tseng <wftseng@gl-login2.arc-ts.umich.edu>
2021-11-17 17:18:04 -0500

    debuggincudamalloc bug

--- project.cu
+++ project.cu
@@ -72,62 +72,62 @@ int main(int argc, char const *argv[]) {
 	
 	//CUDA Code section
 	//1st aggregation start//
-	printf("check0\n");
+	// printf("check0\n");
 	int feature_num = GCN_c.feature_c.feature_num;
 	int node_num = GCN_c.feature_c.node_num;
 	int indexs_num = GCN_c.spec_c.nodes+1;
 	int neighbours_num = GCN_c.spec_c.edges;
 	cudaMalloc(&device_graph_c, sizeof(graph_t));
-	cudaMalloc(&device_graph_c->indexes, indexs_num * sizeof(int));
+	cudaMalloc(&(device_graph_c->indexes), indexs_num * sizeof(int));
 	cudaMalloc(&device_graph_c->neighbours, neighbours_num * sizeof(int));
 	cudaMemcpy(device_graph_c, &GCN_c.graph_c, sizeof(graph_t), cudaMemcpyHostToDevice);
 	cudaMemcpy(device_graph_c->indexes, GCN_c.graph_c.indexes, indexs_num * sizeof(int), cudaMemcpyHostToDevice);
 	cudaMemcpy(device_graph_c->indexes, GCN_c.graph_c.indexes, indexs_num * sizeof(int), cudaMemcpyHostToDevice);
 	printf("check, feature num = %d, node num = %d\n", feature_num, node_num);
-	cudaMalloc(&device_in_feature_c, sizeof(feature_t));
-	cudaMalloc(&device_in_feature_c->features,  feature_num * node_num * sizeof(float));
-	printf("check1\n");
-	cudaMemcpy(device_in_feature_c, &GCN_c.feature_c, sizeof(feature_t), cudaMemcpyHostToDevice);
-	printf("check2\n");
-	for(int i=0; i<10; ++i)
-		printf("%f ", GCN_c.feature_c.features[i]);
-	printf("\n");
-	cudaMemcpy(device_in_feature_c->features, GCN_c.feature_c.features, feature_num * node_num * sizeof(float), cudaMemcpyHostToDevice);
-	printf("check3\n");
-	cudaMalloc(&device_out_feature_c, sizeof(feature_t));
-	cudaMalloc(&device_out_feature_c->features,  feature_num * node_num * sizeof(float));
+	// cudaMalloc(&device_in_feature_c, sizeof(feature_t));
+	// cudaMalloc(&device_in_feature_c->features,  feature_num * node_num * sizeof(float));
+	// printf("check1\n");
+	// cudaMemcpy(device_in_feature_c, &GCN_c.feature_c, sizeof(feature_t), cudaMemcpyHostToDevice);
+	// printf("check2\n");
+	// for(int i=0; i<10; ++i)
+	// 	printf("%f ", GCN_c.feature_c.features[i]);
+	// printf("\n");
+	// cudaMemcpy(device_in_feature_c->features, GCN_c.feature_c.features, feature_num * node_num * sizeof(float), cudaMemcpyHostToDevice);
+	// printf("check3\n");
+	// cudaMalloc(&device_out_feature_c, sizeof(feature_t));
+	// cudaMalloc(&device_out_feature_c->features,  feature_num * node_num * sizeof(float));
 
-	host_out_feature_c = (feature_t*)malloc(sizeof(feature_t));
-	host_out_feature_c->features = (float*)malloc(feature_num * node_num * sizeof(float));
-	printf("check4\n");
-	int block_size = 128;
-	int gdx = feature_num/block_size;
-	if(feature_num % block_size != 0) gdx++;
-	dim3 gd(gdx, 1, 1);
-	dim3 bd(block_size, 1, 1);
-	aggregation_gpu<<<gd, bd>>>(device_graph_c, device_in_feature_c, device_out_feature_c);
-	printf("check5\n");
-	cudaError_t err = cudaGetLastError();
+	// host_out_feature_c = (feature_t*)malloc(sizeof(feature_t));
+	// host_out_feature_c->features = (float*)malloc(feature_num * node_num * sizeof(float));
+	// printf("check4\n");
+	// int block_size = 128;
+	// int gdx = feature_num/block_size;
+	// if(feature_num % block_size != 0) gdx++;
+	// dim3 gd(gdx, 1, 1);
+	// dim3 bd(block_size, 1, 1);
+	// aggregation_gpu<<<gd, bd>>>(device_graph_c, device_in_feature_c, device_out_feature_c);
+	// printf("check5\n");
+	// cudaError_t err = cudaGetLastError();
 
-     if ( err != cudaSuccess )
-     {
-        printf("CUDA Error: %s\n", cudaGetErrorString(err));       
+    //  if ( err != cudaSuccess )
+    //  {
+    //     printf("CUDA Error: %s\n", cudaGetErrorString(err));       
 
-        // Possibly: exit(-1) if program cannot continue....
-     }
-	cudaDeviceSynchronize();
-	//copy back to host
-	cudaMemcpy(host_out_feature_c, device_out_feature_c, sizeof(feature_t), cudaMemcpyDeviceToHost);
-	cudaMemcpy(host_out_feature_c->features, device_out_feature_c->features, feature_num * node_num * sizeof(float), cudaMemcpyDeviceToHost);
-	// output the result to file to check the correctness
-	FILE *result_file;
-	result_file = fopen("/home/wftseng/EECS598_GPU_Final_Project/eecs598/result/first_aggregation_result_gpu", "w+");
-	if(result_file == NULL)
-		printf("error opening file in ./result/first_aggregation_result_gpu\n");
-	for(int i=0; i<feature_num*node_num; ++i)
-		fprintf(result_file, "%f\n", host_out_feature_c->features[i]);
-	fclose(result_file);
-	printf("finish_writing the result\n");
+    //     // Possibly: exit(-1) if program cannot continue....
+    //  }
+	// cudaDeviceSynchronize();
+	// //copy back to host
+	// cudaMemcpy(host_out_feature_c, device_out_feature_c, sizeof(feature_t), cudaMemcpyDeviceToHost);
+	// cudaMemcpy(host_out_feature_c->features, device_out_feature_c->features, feature_num * node_num * sizeof(float), cudaMemcpyDeviceToHost);
+	// // output the result to file to check the correctness
+	// FILE *result_file;
+	// result_file = fopen("/home/wftseng/EECS598_GPU_Final_Project/eecs598/result/first_aggregation_result_gpu", "w+");
+	// if(result_file == NULL)
+	// 	printf("error opening file in ./result/first_aggregation_result_gpu\n");
+	// for(int i=0; i<feature_num*node_num; ++i)
+	// 	fprintf(result_file, "%f\n", host_out_feature_c->features[i]);
+	// fclose(result_file);
+	// printf("finish_writing the result\n");
 	//1st aggregation end//
 
 	//Add commands for profiling -> Look into this -> Lec 17.pdf
